bdd817754840e1f57048ea624a47f736a0469470
Srihari Konakanchi <contactsrihari@gmail.com>
2014-08-25 17:28:23 +0100

    first github commit

---
+++ src/com/git/TestGit.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.git;
+
+/**
+ *
+ * @author srihari konakanchi
+ */
+public class TestGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("thanks gitgub!");
+    }
+    
+}
